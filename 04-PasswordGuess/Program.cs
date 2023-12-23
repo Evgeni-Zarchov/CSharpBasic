3b1fccb405b1969c83227cac515a064acd3dd54c
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-12-23 16:50:37 +0200

    ConditionalStatements

---
+++ 04-PasswordGuess/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _04_PasswordGuess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // so i have to write a program to login with the given password s3cr3t!P@ssw0rd and to show Welcome
+            // if the password doesnt match the given password Wrong password!
+
+            string password = Console.ReadLine();
+
+            if (password == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
---
+++ 06-SpeedInfo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _06_SpeedInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // so i have to write a program to follow the speed 
+            // if the speed is 10 inclusive output "slow"
+            // if the speed is over 10 and 50 inclusive output "average"
+            // if the speed is over 50 and 150 inclusive output "fast"
+            // if the speed is over 150 and to 1000 inclusive output "ultra fast"
+            // if the speedi is over 1000 output "extremely fast"
+
+            double speed = double.Parse(Console.ReadLine());
+
+            if (speed <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (speed <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if (speed <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (speed <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
